11fff13dd82116f5913f4b9a670d9286120162c7
Nishi Lad <nishilad2208@gmail.com>
2019-10-19 15:14:22 -0500

    added lists

--- src/components/fragmentDemo.js
+++ src/components/fragmentDemo.js
@@ -7,6 +7,12 @@ class FragmentDemo extends React.Component{
             <React.Fragment>
                 <h1>Fragment Demo</h1>
                 <p> This describes the Fragment demo</p>
+                <ul>
+                    <li>List 1</li>
+                    <li>List 2</li>
+                    <li>List 3</li>
+                    <li>List 4</li>
+                </ul>
             </React.Fragment>
                 
             
